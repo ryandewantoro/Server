48768657a8ff58fc03d364916c6d66f105aa0c01
Ryan Dewantoro <riiean@umsida.ac.id>
2018-12-17 19:23:56 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+apt-get update
+apy-get dist-upgrade
+apt-get install apache2 php5 libapache2-mod-php5 php5-mcrypt php5-mysql php5-common php5-cli
+php5enmod mcrypt
+service apache2 restart
+apt-get install php5-curl php5-gd php5-snmp snmp curl git
+apt-get install mysql-server
+apt-get install phpmyadmin
