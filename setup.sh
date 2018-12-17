d57d66514c03f042b3718f421ad2c7abce670988
Ryan Dewantoro <riiean@umsida.ac.id>
2018-12-17 22:36:16 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
-apt-get update
-apy-get dist-upgrade
+apt-get update && apt-get upgrade
 apt-get install apache2 php5 libapache2-mod-php5 php5-mcrypt php5-mysql php5-common php5-cli
 php5enmod mcrypt
 service apache2 restart
