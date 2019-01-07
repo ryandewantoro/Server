3c77eb24d94e8a25eb8541aa94ce539d9dd64072
Ryan Dewantoro <riiean@umsida.ac.id>
2019-01-07 12:41:13 +0700

    Create php.sh

---
+++ php.sh
@@ -0,0 +1,6 @@
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
+sudo apt-get install php5.6 php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml
+sudo a2dismod php5
+sudo a2enmod php5.6
+sudo service apache2 restart
