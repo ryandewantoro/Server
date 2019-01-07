c3232219e69649b03e357478f0d0544a8ebeab3b
Ryan Dewantoro <riiean@umsida.ac.id>
2019-01-07 20:41:19 +0700

    Update php.sh

--- php.sh
+++ php.sh
@@ -1,12 +1,7 @@
+sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
 sudo apt-get install php5.6 php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml
 sudo a2dismod php5
 sudo a2enmod php5.6
 sudo service apache2 restart
-
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:ondrej/php5-5.6
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install php5
