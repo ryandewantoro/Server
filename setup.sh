d70a3de3dcffa7c31992336ae9ae744bc5ca6c7b
Ryan Dewantoro <riiean@umsida.ac.id>
2019-03-01 10:49:25 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,12 +9,12 @@ apt-get install mysql-server
 apt-get install phpmyadmin
 #apt-get install samba
 apt-get install postgresql postgresql-contrib
-mkdir /usr/share/adminer
-wget "http://www.adminer.org/latest.php" -O /usr/share/adminer/latest.php
-ln -s /usr/share/adminer/latest.php /usr/share/adminer/adminer.php
-echo "Alias /adminer.php /usr/share/adminer/adminer.php" | sudo tee /etc/apache2/conf-available/adminer.conf
-a2enconf adminer.conf
-systemctl reload apache2
-echo "Visit adminer http://<Server_IP_or_Domain>/adminer.php";
+#mkdir /usr/share/adminer
+#wget "http://www.adminer.org/latest.php" -O /usr/share/adminer/latest.php
+#ln -s /usr/share/adminer/latest.php /usr/share/adminer/adminer.php
+#echo "Alias /adminer.php /usr/share/adminer/adminer.php" | sudo tee /etc/apache2/conf-available/adminer.conf
+#a2enconf adminer.conf
+#systemctl reload apache2
+#echo "Visit adminer http://<Server_IP_or_Domain>/adminer.php";
 echo "Install Selesai.......";
 #sh setting.sh
