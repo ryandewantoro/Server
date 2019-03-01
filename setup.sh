1b1699484c3c28cac10447470b73437fb7588bff
Ryan Dewantoro <riiean@umsida.ac.id>
2019-03-01 11:34:13 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ apt-get update && apt-get upgrade
 apt-get install apache2 php5 libapache2-mod-php5 php5-mcrypt php5-mysql php5-common php5-cli
 php5enmod mcrypt
 service apache2 restart
-apt-get install php5-curl php5-gd php5-snmp snmp curl git
+apt-get install php5-curl php5-gd php5-snmp snmp curl git php5-pgsql
 apt-get install mysql-server
 apt-get install phpmyadmin
 #apt-get install samba
