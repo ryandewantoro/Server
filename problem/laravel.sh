29455ddcda38da6fca541272feacd2329e0c0048
Ryan Dewantoro <riiean@umsida.ac.id>
2020-02-08 21:54:09 +0700

    Update laravel.sh

--- problem/laravel.sh
+++ problem/laravel.sh
@@ -2,6 +2,8 @@
 apt-get install phpunit
 apt-get install php-mbstring
 apt-get install unzip
+#sesuaikan dengan versi PHP
 apt-get install php-gd
 apt-get install php-intl
 apt-get install php-xsl
+echo "selesai";
