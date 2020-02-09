a227c563853c3f6ac7e3d3a427846edbe8608bdf
Ryan Dewantoro <riiean@umsida.ac.id>
2020-02-09 11:40:06 +0700

    Update php-fpm.sh

--- php-fpm.sh
+++ php-fpm.sh
@@ -11,18 +11,15 @@ echo "<VirtualHost *:80>
     ServerName airprigen.com
     ServerAlias www.airprigen.com
     DocumentRoot /var/www/html/airprigen/public_html/
- 
     <Directory /var/www/html/airprigen/public_html/>
         Options -Indexes +FollowSymLinks +MultiViews
         AllowOverride All
         Require all granted
     </Directory>
- 
     <FilesMatch \.php$>
         # 2.4.10+ can proxy to unix socket
         SetHandler "proxy:unix:/var/run/php/php7.3-fpm.sock|fcgi://localhost"
     </FilesMatch>
- 
     ErrorLog /var/www/html/airprigen/public_html/logs/error.log
     CustomLog /var/www/html/airprigen/public_html/logs/access.log combined
 </VirtualHost>" > /etc/apache2/sites-available/airprigen.com.conf
