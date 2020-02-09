455fbb0d74e4a8cba00e1a865a1326050ba7f8ef
Ryan Dewantoro <riiean@umsida.ac.id>
2020-02-09 11:28:54 +0700

    Update php-fpm.sh

--- php-fpm.sh
+++ php-fpm.sh
@@ -5,5 +5,23 @@ apt install software-properties-common | add-apt-repository ppa:ondrej/php
 apt update
 apt install php7.3 php7.3-fpm
 a2enmod actions fcgid alias proxy_fcgi
-
+echo "<VirtualHost *:80>
+    ServerName airprigen.com
+    ServerAlias www.airprigen.com
+    DocumentRoot /var/www/html/airprigen/public_html/
+ 
+    <Directory /var/www/html/airprigen/public_html/>
+        Options -Indexes +FollowSymLinks +MultiViews
+        AllowOverride All
+        Require all granted
+    </Directory>
+ 
+    <FilesMatch \.php$>
+        # 2.4.10+ can proxy to unix socket
+        SetHandler "proxy:unix:/var/run/php/php7.3-fpm.sock|fcgi://localhost"
+    </FilesMatch>
+ 
+    ErrorLog /var/www/html/airprigen/public_html/logs/error.log
+    CustomLog /var/www/html/airprigen/public_html/logs/access.log combined
+</VirtualHost>" > /etc/apache2/sites-available/airprigen.com.conf
 echo "<?php phpinfo(); ?>" > /var/www/html/info.php
