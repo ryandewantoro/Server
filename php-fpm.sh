bc4f5c6ee2fd64d00afa92971f8e8fb1f1b816c3
Ryan Dewantoro <riiean@umsida.ac.id>
2020-02-12 08:09:22 +0700

    Update php-fpm.sh

--- php-fpm.sh
+++ php-fpm.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 apt update
 apt install apache2 libapache2-mod-fcgid
-apt install software-properties-common | add-apt-repository ppa:ondrej/php
-apt update
+apt install software-properties-common | add-apt-repository ppa:ondrej/php | apt update
 apt install php7.3 php7.3-fpm
 a2enmod actions fcgid alias proxy_fcgi
 mkdir /var/www/html/airprigen | /var/www/html/airprigen/public_html | /var/www/html/airprigen/logs
