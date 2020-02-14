b391df66a25faed6683593fa5f5948d4df95485d
Ryan Dewantoro <riiean@umsida.ac.id>
2020-02-14 19:27:04 +0700

    Update laravel.sh

--- problem/laravel.sh
+++ problem/laravel.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+echo "install composer";
+apt install wget php-cli php-zip unzip
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+HASH="$(wget -q -O - https://composer.github.io/installer.sig)"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+
+
 apt-get install phpunit
 apt-get install php-mbstring
 apt-get install unzip
