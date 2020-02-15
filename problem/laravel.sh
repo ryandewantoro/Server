a71d5b38ca4c977d2801aa9e18b8f096cc9cc1d8
Ryan Dewantoro <riiean@umsida.ac.id>
2020-02-15 17:49:53 +0700

    Update laravel.sh

--- problem/laravel.sh
+++ problem/laravel.sh
@@ -6,6 +6,8 @@ HASH="$(wget -q -O - https://composer.github.io/installer.sig)"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
+composer global require laravel/installer
+composer create-project --prefer-dist laravel/laravel blog "5.8.*"
 
 apt-get install phpunit
 apt-get install php-mbstring
