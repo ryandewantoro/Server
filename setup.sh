3c928519f24f4a9cb9f1a8e4b8623dc5e6ef1966
Ryan Dewantoro <riiean@umsida.ac.id>
2020-10-11 21:25:00 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,3 +19,13 @@ a2enmod ssl
 #echo "Visit adminer http://<Server_IP_or_Domain>/adminer.php";
 echo "Install Selesai.......";
 #sh setting.sh
+
+apt-get remove docker docker-engine docker.io
+apt install docker.io
+systemctl start docker
+systemctl enable docker
+docker pull mysql:8.0.1
+docker run --name my-own-mysql -e MYSQL_ROOT_PASSWORD=mypass123 -d mysql:8.0.1
+docker pull phpmyadmin/phpmyadmin:latest
+docker run --name my-own-phpmyadmin -d --link my-own-mysql:db -p 8081:80 phpmyadmin/phpmyadmin
+echo "Install Selesai.......";
