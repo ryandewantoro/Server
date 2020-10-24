8d6626c1bcebd74a2e69ee09a641f9b36d56f469
Ryan Dewantoro <riiean@umsida.ac.id>
2020-10-24 09:49:17 +0700

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -14,17 +14,18 @@ usermysql="root"
 passwordmysql="1912"
 
 #komunikasi
-apt install sshpass
+sudo apt install sshpass
 sshpass -p '$password' ssh $user@$host
 
 #install dan konfigurasi database
 echo "program di mulai";
-apt-get install mysql-server mysql-client
+sudo apt-get install mysql-server mysql-client
 echo "menulis script di my.cnf";
 echo "server-id              = 1
 log_bin                = /var/log/mysql/mysql-bin.log
 binlog_do_db           = db_umsida
 #bind-address          = 127.0.0.1" >> /etc/mysql/my.cnf
+
 echo "restart mysql";
 /etc/init.d/mysql restart
 
@@ -37,14 +38,15 @@ exit;
 
 echo "pindah ke server B";
 sshpass -p '$passwordb' ssh $userb@$hostb
-
-apt-get install mysql-server mysql-client
-
-server-id              = 2
+echo "program di mulai";
+sudo apt-get install mysql-server mysql-client
+echo "menulis script di my.cnf";
+echo "server-id              = 2
 log_bin                = /var/log/mysql/mysql-bin.log
 binlog_do_db           = db_umsida
-# bind-address         = 127.0.0.1
+# bind-address         = 127.0.0.1" >> /etc/mysql/my.cnf
 
+echo "restart mysql";
 /etc/init.d/mysql restart
 
 mysql -u root -proot
@@ -58,9 +60,12 @@ CHANGE MASTER TO MASTER_HOST = '192.168.10.22', MASTER_USER = 'replicator', MAST
 slave start;
 
 show master status;
+exit;
+
 
 echo "pindah ke server A";
-
+sshpass -p '$password' ssh $user@$host
+mysql -u $root -p$passwordmysql
 slave stop; 
 CHANGE MASTER TO MASTER_HOST = '192.168.10.23', MASTER_USER = 'replicator', MASTER_PASSWORD = 'root', MASTER_LOG_FILE = 'mysql-bin.000002', MASTER_LOG_POS = 109; 
 slave start;
@@ -68,7 +73,8 @@ slave start;
 create database contohdb;
 create table contohdb.dummy (`id` varchar(10));
 show tables in contohdb;
+exit;
 
+# sumber
 # link https://linux.tutorials.id/cara-setting-dan-syncronize-mysql-master-master/
-
 #http://www.clusterdb.com/mysql-cluster/get-mysql-replication-up-and-running-in-5-minutes
