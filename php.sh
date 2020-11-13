29d5a6e3e05fd09093738a540f7d952fda2abf23
Ryan Dewantoro <riiean@umsida.ac.id>
2020-11-13 11:22:23 +0700

    Delete php.sh

--- php.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:ondrej/php
-sudo apt-get update
-sudo apt-get install php5.6 php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml
-sudo a2dismod php5
-sudo a2enmod php5.6
-sudo service apache2 restart
