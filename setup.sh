1fd5366da04d5691d521b89ad27e2ed5084d20fb
Ryan Dewantoro <riiean@umsida.ac.id>
2020-12-02 12:02:52 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -90,8 +90,6 @@ echo "
 
 
 # 5 cronjob
-echo "* 1 * * * chmod -R 777 /var/www/html/" > /etc/cron.d/jadwal >> /var/log/cron.log
-echo "* 1 * * * chmod -R 777 /var/www/html/" > /etc/cron.hourly/jadwal >> /var/log/cron.log
 echo "* 1 * * * chmod -R 777 /var/www/html/" > /var/spool/cron/crontabs/root
 
 # cara lihat hasil log cronjob
