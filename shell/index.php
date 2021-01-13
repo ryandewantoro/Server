699038353c291c2daa87f3f8d0d32860c9b816e8
Ryan Dewantoro <riiean@umsida.ac.id>
2021-01-13 12:13:27 +0700

    Update index.php

--- shell/index.php
+++ shell/index.php
@@ -24,10 +24,8 @@ echo "<hr />";
 //echo "<pre>$output</pre>";
 ?>
 
-<form action="/action_page.php">
-<label for="w3review">Review of W3Schools:</label>
-<textarea id="w3review" name="w3review" rows="4" cols="50">
-  At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.
+<form action="">
+<textarea id="" name="" rows="4" cols="50">
   </textarea>
   <br><br>
   <input type="submit" value="Submit">
