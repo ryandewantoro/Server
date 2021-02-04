6e56896e4ba8d5d4a7f2d62b1e8a61fdd52a5385
Ryan Dewantoro <riiean@umsida.ac.id>
2021-02-04 14:55:22 +0700

    Update index.php

--- php/api/index.php
+++ php/api/index.php
@@ -1,2 +1,12 @@
-<?php
-?>
+Consumer API keys
+
+API key:
+GbT491MaPGWZ7uAGLdfcTebwA
+
+API secret key:
+agoZJ7fJ5K7cTA6bNasFPDSZnyf7P6OIMu7L5pe1L3VO9ykTTj
+
+
+Access token : 265901279-A7nGy8eqIrY45ys7cGD9ltfeIsNU8X7aiOCn0fh1
+
+Access token secret : XHizmCL0dUSJ53pwsPWUea9xvQvfP1HBTjkS2VrYWY0XY
