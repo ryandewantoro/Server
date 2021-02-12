2b79182a3ca0bc516406e485fbc3b42a7e8b7dd4
Ryan Dewantoro <riiean@umsida.ac.id>
2021-02-12 12:56:52 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -117,5 +117,5 @@ docker pull portainer/portainer
 docker pull phpmyadmin/phpmyadmin:latest
 docker run --name my-own-mysql -e MYSQL_ROOT_PASSWORD=mypass123 -d mysql:8.0.1
 docker run --name my-own-phpmyadmin -d --link my-own-mysql:db -p 8081:80 phpmyadmin/phpmyadmin
-docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer
+docker run -d -p 9000:9000 -v --restart=always /var/run/docker.sock:/var/run/docker.sock portainer/portainer
 echo "Install Selesai.......";
