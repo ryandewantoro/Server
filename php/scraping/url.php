d788a943ad37b84306100e8d1d005c37c44aba18
Ryan Dewantoro <riiean@umsida.ac.id>
2021-08-12 17:10:07 +0700

    Create url.php

---
+++ php/scraping/url.php
@@ -0,0 +1,11 @@
+<?php
+include_once('../simple_html_dom.php');
+$target = 'http://www.beritaterheboh.com';
+$html = file_get_html($target);
+
+// link url
+$list = $html->find('[class="item-title"]');
+foreach ( $list as $item ) {
+    echo $item->outertext;
+}
+?>
