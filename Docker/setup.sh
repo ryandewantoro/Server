d16eb12feba1454606c9848f0b52f50c1775135b
Ryan Dewantoro <riiean@umsida.ac.id>
2021-09-25 06:45:58 +0700

    Update setup.sh

--- Docker/setup.sh
+++ Docker/setup.sh
@@ -5,6 +5,7 @@ systemctl start docker
 systemctl enable docker
 docker pull mysql:8.0.1
 docker pull portainer/portainer
+docker pull pihole/pihole
 docker pull phpmyadmin/phpmyadmin:latest
 docker run --name my-own-mysql -e MYSQL_ROOT_PASSWORD=mypass123 -d mysql:8.0.1
 docker run --name my-own-phpmyadmin -d --link my-own-mysql:db -p 8081:80 phpmyadmin/phpmyadmin
