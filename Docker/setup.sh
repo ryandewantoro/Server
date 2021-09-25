9c0fba445fc7b7bd679a465324ed39f816fc3bd9
Ryan Dewantoro <riiean@umsida.ac.id>
2021-09-25 07:25:46 +0700

    Update setup.sh

--- Docker/setup.sh
+++ Docker/setup.sh
@@ -10,4 +10,8 @@ docker pull phpmyadmin/phpmyadmin:latest
 docker run --name my-own-mysql -e MYSQL_ROOT_PASSWORD=mypass123 -d mysql:8.0.1
 docker run --name my-own-phpmyadmin -d --link my-own-mysql:db -p 8081:80 phpmyadmin/phpmyadmin
 docker run -d --restart=always -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer
+
+docker volume create pihole
+docker volume create dnsmasq
+docker run --name=pihole -e TZ=America/New_York -e WEBPASSWORD=YOURPASS -e SERVERIP=YOUR.SERVER.IP -v pihole:/etc/pihole -v dnsmasq:/etc/dnsmasq.d -p 80:80 -p 53:53/tcp -p 53:53/udp --restart=unless-stopped pihole/pihole
 echo "Install Selesai.......";
