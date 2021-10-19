6516ea49110d53743a2a8b7dcc45f6d7bb058fe4
Ryan Dewantoro <riiean@umsida.ac.id>
2021-10-19 07:09:55 +0700

    Update index.php

--- php/api/index.php
+++ php/api/index.php
@@ -1,12 +1,16 @@
 Consumer API keys
-
-API key:
-GbT491MaPGWZ7uAGLdfcTebwA
-
-API secret key:
-agoZJ7fJ5K7cTA6bNasFPDSZnyf7P6OIMu7L5pe1L3VO9ykTTj
-
-
+API key: GbT491MaPGWZ7uAGLdfcTebwA
+API secret key: agoZJ7fJ5K7cTA6bNasFPDSZnyf7P6OIMu7L5pe1L3VO9ykTTj
 Access token : 265901279-A7nGy8eqIrY45ys7cGD9ltfeIsNU8X7aiOCn0fh1
-
 Access token secret : XHizmCL0dUSJ53pwsPWUea9xvQvfP1HBTjkS2VrYWY0XY
+
+//twitter
+$token = "265901279-SCXOXWBjbaKtOlo9DWoRvMPNrGqaYOvTIpJ9NZZl";
+$token_secret = "ppErJfa88VRhZNVZEgkEdqubiTHhNndLjIwAdx6oZc1Uz";
+$consumer_key = "obdTWtNjyWUaE0rQfSC6I70Ah";
+$consumer_secret = "j94JZxoTwjzkQcttnySoeSQFzFz6NbfTIB281fZ6WGbqsQZ7lL";
+//twitter
+$token = "1085506165939699712-bauh5z8vIH0LIB8mt8f6TgqmGTHGIv";
+$token_secret = "fUIDQHFTfWMY6iCRxjTrQ9Wfhm5PGCmQrBh2fSZlEtkih";
+$consumer_key = "hBRDxLme99SK8OFPbMEaBRU1t";
+$consumer_secret = "kAQ7KfO351VYqFCASxxMjjdteiffkhafAmp9ayv9expeMM17j3";
